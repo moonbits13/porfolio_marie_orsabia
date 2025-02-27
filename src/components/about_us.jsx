2e5976cd406087555c06525607eadecdc724188c
Relloliza <iamrelloliza@gmail.com>
2025-02-27 21:25:00 +0800

    Updated styles for mobile responsiveness

--- src/components/about_us.jsx
+++ src/components/about_us.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
 import catto from "../assets/catto.png";
-import me from "../assets/me-as-cat.jpg";
 import veronlogo from "../assets/veron-logo.png";
+import first from "../assets/first.png";
+import second from "../assets/second.png";
+import third from "../assets/third.png";
 
 const AboutUs = () => {
   return (
@@ -24,8 +26,32 @@ const AboutUs = () => {
            </div>
          </nav>
 
-         <div>hello philippines try lang mewmewewew</div>
+         <div className="container-2">
+        <div className="col-1">ABOUT ME</div>
+      </div>
+
+         <div class="container-about">
+        
+        <div class="first-row">
+            <div class="box">
+              <img src={first} alt="curious catto"  />
+            </div>
+            <div class="box">
+              <img src={second} alt="eepy catto"  />
+            </div>
+            <div class="box">
+              <img src={third} alt="girl boss catto"  />
+            </div>
+        </div>
+
+       
+        <div class="second-row">
+            <p>I'm just a curious and tired soul, navigating life one mediocre task at a time, secretly hoping it all adds up to something great. I dream of being my own boss,
+               even if it means stumbling through the process with caffeine and determination. Here's to figuring it out, one step at a time.</p>
+        </div>
+    </div>
        </div>
+       
   );
 };
 
